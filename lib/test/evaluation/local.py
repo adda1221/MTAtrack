1fe38b08a81977f269b7742548e818b0ed7c86e1
adda1221 <114666579+adda1221@users.noreply.github.com>
2022-10-05 17:49:29 +0800

    Add files via upload

---
+++ lib/test/evaluation/local.py
@@ -0,0 +1,31 @@
+from lib.test.evaluation.environment import EnvSettings
+
+def local_env_settings():
+    settings = EnvSettings()
+
+    # Set your local paths here.
+
+    settings.davis_dir = ''
+    settings.got10k_lmdb_path = '/root/projects/MAtrack/data/got10k_lmdb'
+    settings.got10k_path = '/root/projects/TransT/data/GOT10k'
+    settings.got_packed_results_path = ''
+    settings.got_reports_path = ''
+    settings.lasot_lmdb_path = '/root/projects/MAtrack/data/lasot_lmdb'
+    settings.lasot_path = '/root/projects/TransT/data/Lasot/LaSOTBenchmark'
+    settings.network_path = '/root/projects/MAtrack/test/networks'    # Where tracking networks are stored.
+    settings.nfs_path = ''
+    settings.otb_path = ''
+    settings.prj_dir = '/root/projects/MAtrack'
+    settings.result_plot_path = '/root/projects/MAtrack/test/result_plots'
+    settings.results_path = '/root/projects/MAtrack/test/tracking_results'    # Where to store tracking results
+    settings.save_dir = '/root/projects/MAtrack'
+    settings.segmentation_path = '/root/projects/MAtrack/test/segmentation_results'
+    settings.tn_packed_results_path = ''
+    settings.tpl_path = ''
+    settings.trackingnet_path = '/root/projects/TransT/data/TrackingNet'
+    settings.uav_path = ''
+    settings.vot_path = '/root/projects/MAtrack/data/VOT2019'
+    settings.youtubevos_dir = ''
+
+    return settings
+
