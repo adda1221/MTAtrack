b0afc0c7820e36bd7f62593cb0c9891cd5bdd237
adda1221 <114666579+adda1221@users.noreply.github.com>
2023-05-14 16:44:45 +0800

    Update analysis_results.py
    Add UAV123 and OTB100 datasets test support.

--- tracking/analysis_results.py
+++ tracking/analysis_results.py
@@ -6,13 +6,13 @@ from lib.test.analysis.plot_results import plot_results, print_results, print_pe
 from lib.test.evaluation import get_dataset, trackerlist
 
 trackers = []
+dataset_name = 'lasot' #lasot,uav,otb
 
-
-trackers.extend(trackerlist(name='MAtrack_st', parameter_name='baseline', dataset_name='lasot',
+trackers.extend(trackerlist(name='MAtrack_st', parameter_name='baseline', dataset_name=dataset_name,
                             run_ids=None, display_name='MAtrack-ST'))
 
-dataset = get_dataset('lasot')
-plot_results(trackers, dataset, 'LaSOT', merge_results=True, plot_types=('success', 'norm_prec'),
+dataset = get_dataset(dataset_name)
+plot_results(trackers, dataset, dataset_name, merge_results=True, plot_types=('success', 'norm_prec'),
              skip_missing_seq=False, force_evaluation=True, plot_bin_gap=0.05)
 print_results(trackers, dataset, 'LaSOT', merge_results=True, plot_types=('success', 'prec', 'norm_prec'))
 # print_per_sequence_results(trackers, dataset, report_name="debug")
